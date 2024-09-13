3728fce3cf77beb375121d29ac2dc72a8f95482b
Andrey Veselov <lifeforglory@gmail.com>
2024-09-13 11:22:10 +0300

    build: quiz-class

---
+++ quizClass.js
@@ -0,0 +1,61 @@
+const readline = require("readline");
+const chalk = require("chalk");
+
+// --------------------------------------------------------------------------------
+
+class Quiz {
+  constructor(questions) {
+    this.questions = questions;
+    this.score = 0;
+    this.currentQuestionIndex = 0;
+
+    // Настраиваем интерфейс для взаимодействия с пользователем
+
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  }
+
+  // Запуск квиза
+
+  start() {
+    console.log("Добро пожаловать в квиз! Ответы должны быть 'да' или 'нет'.");
+    this.askNextQuestion();
+  }
+
+  // Задание следующего вопроса
+
+  askNextQuestion() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      const currentQuestion = this.questions[this.currentQuestionIndex];
+      this.rl.question(currentQuestion.text + " ", (answer) => {
+        // Проверяем ответ пользователя
+        if (currentQuestion.checkAnswer(answer)) {
+          console.log("Правильно! 👍💃🏻");
+          this.score++;
+        } else {
+          console.log("Неправильно! 💩");
+        }
+
+        // Переходим к следующему вопросу
+        this.currentQuestionIndex++;
+        this.askNextQuestion();
+      });
+    } else {
+      // Если вопросы закончились, завершаем квиз
+
+      this.endQuiz();
+    }
+  }
+
+  // Завершение квиза
+  endQuiz() {
+    console.log(
+      `Квиз завершён! Ваш результат: ${this.score} из ${this.questions.length}`
+    );
+    this.rl.close();
+  }
+}
+
+module.exports = Quiz;
--- stash.js
+++ stash.js
@@ -6,44 +6,35 @@ const { EOL } = require("os");
 class CardQuestion {
 
     static async readeFileFunc() {
-try{
+
     const pathQuest = "./topics/";
     const files1 = await fs.readdir(pathQuest);
     const readQuest = await fs.readFile(pathQuest + files1[0], "utf8");
     const newArr1 = readQuest.split('\n');
     return newArr1
    
-}
-catch (error) {
-    console.error("Ошибка при чтении файлов:", error);
-  }
     }
 }
-// --------------------------------------------------
+// ------------------------------------------------------------
 
 class CardAnswer {
 
     static async readeFileFunc() {
-try{
+
     
     const pathAnswer = "./answer/";
     const files2 = await fs.readdir(pathAnswer);
     const readAnswer = await fs.readFile(pathAnswer + files2[0], "utf8");
     const newArr2 = readAnswer.split('\n');
     return newArr2
-}
-catch (error) {
-    console.error("Ошибка при чтении файлов:", error);
-  }
+
     }
 }
-
+// -------------------------------------------------------------------
 (async ()=> {
 
     try{
-    CardQuestion.readeFileFunc();
-    CardAnswer.readeFileFunc();
-    
+     
     inquirer
       .prompt([
         { type: "input", name: "username", message: "Квиз Квиз Квиз !" },
@@ -103,3 +94,4 @@ catch (error) {
         console.log("errorr")
     }
 })
+// ------------------------------------------------------------------
